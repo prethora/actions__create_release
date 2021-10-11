dd544903b39dcc3d48ccd1b132f4f05ace3c19fa
Dominique Adolphe <domiado16@gmail.com>
2021-10-11 10:28:37 +0400

    2021-10-11-10-28

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Create Release javascript action
 
-This action ...
+This action creates a release from a push with a tag reference.
 
 ## Inputs
 
@@ -10,12 +10,16 @@ This action ...
 
 ## Outputs
 
-## `time`
+## `response`
 
-The time we greeted you.
+The response from the successful create release api call.
 
 ## Example usage
 
-    uses: prethora/actions__create_release@v1.0
-    with:
-      token: ${{secrets.GITHUB_TOKEN}}
+    - name: Create Release  
+      id: create_release
+      uses: prethora/actions__create_release@v1.0
+      with:
+        token: ${{secrets.GITHUB_TOKEN}}
+    - name: Get the response
+      run: echo "The response: ${{ steps.create_release.outputs.response }}"
--- index.js
+++ index.js
@@ -1,16 +1,94 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-// const token = (process.env.LOCAL)?process.env.TOKEN:core.getInput("token");
-// const octokit = github.getOctokit(token);        
+const token = (process.env.LOCAL)?process.env.TOKEN:core.getInput("token");
+const octokit = github.getOctokit(token);        
 
 const { ref,repository: { owner: { name: owner }, name: repo } } 
     = (process.env.LOCAL)?require("./local-payload.json"):github.context.payload;
-// const tag_name = ((ref) && (ref.startsWith("refs/tags/")))?ref.substr(10):null;    
+const tag_name = ((ref) && (ref.startsWith("refs/tags/")))?ref.substr(10):null;
+const version = tag_name?tag_name.substr(1):null;
+
+if (process.env.LOCAL)
+{
+    core.setOutput = (name,value) => console.log(name,value);
+    octokit.rest.repos.createRelease = (opt) => ({type: "simulated",opt});
+}
+
+/// Extracts the lines from the CHANGELOG.md (if it exists) in the section matching
+/// `version`
+async function extractChanges(content,version)
+{
+    try
+    {
+        const ret = [];
+        let recording = false;
+        content.split("\n").forEach((line) => 
+        {
+            const res = /^\s*##\s+v?(\d+\.\d+\.\d+)\s*$/.exec(line);
+            if (res)
+            {
+                recording = (res[1]==version);
+            }
+            else if (recording)
+            {
+                ret.push(line);
+            }        
+        });
+        return ret.join("\n").trim();    
+    }
+    catch(err)
+    {
+        console.log("error: ",err);
+        return "";
+    }
+}
+
+/// Extract the content of the file at `path` in the repo as the current reference.
+async function getFileContent(path)
+{
+    try
+    {
+        const res = await octokit.rest.repos.getContent({
+            owner,
+            repo,
+            path
+        });
+        if ((res.status==200) && (res.data))
+        {
+            const buf = Buffer.from(res.data.content,res.data.encoding);
+            return buf.toString("utf8");    
+        }
+        else
+        {
+            return "";
+        }    
+    }
+    catch(err)
+    {
+        console.log("getFileContent error:",err);
+        return "";
+    }
+}
 
 async function run()
 {        
-    // core.setOutput("time",(new Date()).toTimeString());
+    if (ref)
+    {        
+        const res = await octokit.rest.repos.createRelease({
+            owner,
+            repo,
+            tag_name,
+            name: `release ${version}`,
+            body: await extractChanges(await getFileContent("CHANGELOG.md"),version)
+        });
+        core.setOutput("response",res);
+        console.log(`Release '${tag_name}' created successfully`);
+    }
+    else
+    {
+        throw "ref must be a tag";
+    }
 }
 
 run();
